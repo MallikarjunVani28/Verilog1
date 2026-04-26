77dc5df38cf24444552e56cfb6d5d0677e461ceb
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-04-26 10:59:30 +0530

    XOR gate using NOR gates
    Added a testbench for the XOR gate using NOR gates to verify functionality with different input combinations.

---
+++ xor_gate_using_nor_gate.v
@@ -0,0 +1,41 @@
+module nor_gate(input a,b,output y);
+  assign y = ~(a | b);
+endmodule
+
+module xor_gate_using_nor_gate(input a,b,output y);
+  wire w1,w2,w3,w4,w5;
+  
+  nor_gate in1(a,b,w2);
+  nor_gate in2(a,w2,w1);
+  nor_gate in3(w2,b,w3);
+  nor_gate in4(w1,w3,w4);
+  nor_gate in5(w4,w4,w5);
+  
+  
+  assign y = w5;
+endmodule
+
+///testbench
+module tb;
+  reg a,b;
+  wire y;
+  xor_gate_using_nor_gate dut (a,b,y);
+  integer i;
+  
+  initial begin
+    for(i=0;i<4;i=i+1)
+    
+    begin
+      {a,b}= i;
+    #10;
+    end
+  end
+    initial begin
+      #100;
+      $finish;
+    end
+    
+    initial 
+      $monitor("a=%b b=%b,y=%b",a,b,y);
+    endmodule
+    
